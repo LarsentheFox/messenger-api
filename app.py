97e50d0d57c6536dfa73925ee8c8245b2d06d73e
LarsenTheFox <larsencoles02@icloud.com>
2022-07-20 11:57:48 -0400

    MSG0004: adding sqlaclhemy and alembic

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask import Flask
 
+
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] ="" 
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route("/")
 def hello_world():
